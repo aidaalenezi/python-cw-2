6972b5d4beb5544f075312a851a72d4d37261423
aidaalenezi <142677164+aidaalenezi@users.noreply.github.com>
2023-08-28 22:51:14 +0300

    done

---
+++ main.py
@@ -0,0 +1,10 @@
+
+my_name=input('what is your name:')
+my_age=input('what is your age:')
+
+print(f'my name is {my_name} and my age is {my_age}')               
+
+first_number=input('enter your first number')
+secound_number=input('enter your secound number')
+operation=int(first_number)+int(secound_number)
+print(operation)
